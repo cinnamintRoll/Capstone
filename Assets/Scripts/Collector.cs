35dccc8be099cdc23cdf7a39164e7ec3651d7e7c
DESKTOP-VACFDVM\USER-PC <logdatkyle@gmail.com>
2024-09-28 21:55:04 +0800

    floating coin
    - updated collector to have velocity
    - updated punch arrow to use collector velocity
    - updated coin scene

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -7,11 +7,22 @@ using UnityEngine;
 /// </summary>
 public class Collector : MonoBehaviour
 {
+    public Vector3 CollectorVelocity;
     public int TotalItemsCollected = 0; // Total number of collected items
-
+    private Vector3 PreviousPosition;
     public void CollectItem(int itemValue)
     {
         TotalItemsCollected += itemValue;
         Debug.Log("Collected Item! Total Items: " + TotalItemsCollected);
+    }
+    private void Start()
+    {
+        PreviousPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        CollectorVelocity = (transform.position - PreviousPosition) /Time.deltaTime;
+        PreviousPosition = transform.position;
     }
 }
